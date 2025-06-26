769b9d2260cac3f9cb536fd774970fbecf8b87e2
lernzoin <218134394+lernzoin@users.noreply.github.com>
2025-06-26 19:42:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ App.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="text-center text-2xl font-bold text-blue-600 mt-10">
+      Welcome to Lernzo MVP (Live)
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lernzo MVP
+This is the React + Tailwind frontend for Lernzo MVP.
